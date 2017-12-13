de77d01acc81dd4be143205c8effd66e9faf1e2b
Steven1230 <yxb841230@126.com>
2017-12-13 15:30:19 +0800

    middle variable function complete

---
+++ src/main/java/com/cams/blaze/request/CurrentMonthAdjustLimitHistory_Temp.java
@@ -0,0 +1,60 @@
+package com.cams.blaze.request;
+
+import java.util.Date;
+
+/**
+ * @Author XiangbinYuan stevenYuan
+ * @CreationDate 12/13/2017
+ */
+public class CurrentMonthAdjustLimitHistory_Temp {
+
+    private Date adjustLimitDate;
+
+    private Double adjustLimitAmtBefore;
+
+    private Double adjustLimitAmtAfter;
+
+    private String adjustLimitType;
+
+    private Date tempALunValidDate;
+
+    public Date getAdjustLimitDate() {
+        return adjustLimitDate;
+    }
+
+    public void setAdjustLimitDate(Date adjustLimitDate) {
+        this.adjustLimitDate = adjustLimitDate;
+    }
+
+    public Double getAdjustLimitAmtBefore() {
+        return adjustLimitAmtBefore;
+    }
+
+    public void setAdjustLimitAmtBefore(Double adjustLimitAmtBefore) {
+        this.adjustLimitAmtBefore = adjustLimitAmtBefore;
+    }
+
+    public Double getAdjustLimitAmtAfter() {
+        return adjustLimitAmtAfter;
+    }
+
+    public void setAdjustLimitAmtAfter(Double adjustLimitAmtAfter) {
+        this.adjustLimitAmtAfter = adjustLimitAmtAfter;
+    }
+
+    public String getAdjustLimitType() {
+        return adjustLimitType;
+    }
+
+    public void setAdjustLimitType(String adjustLimitType) {
+        this.adjustLimitType = adjustLimitType;
+    }
+
+    public Date getTempALunValidDate() {
+        return tempALunValidDate;
+    }
+
+    public void setTempALunValidDate(Date tempALunValidDate) {
+        this.tempALunValidDate = tempALunValidDate;
+    }
+}
