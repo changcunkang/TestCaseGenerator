967155e4840b96f94365f6a28a8c1855727e1706
Steven1230 <yxb841230@126.com>
2018-03-21 14:17:27 +0800

    middle variable function complete

--- src/test/java/TestMainThread.java
+++ src/test/java/TestMainThread.java
@@ -1,4 +1,6 @@
 import com.cams.blaze.request.Application;
+import com.cams.blaze.request.Customer;
+import com.cams.blaze.request.Product;
 import com.fico.testCaseGenerator.XSTream.XSTreamHelper;
 import com.fico.testCaseGenerator.blazeServer.BlazeServer;
 import com.fico.testCaseGenerator.project.Project;
@@ -6,13 +8,15 @@ import com.fico.testCaseGenerator.repository.ApplicationDao;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class TestMainThread {
 
     public static void main(String[] args) throws Exception{
 
-        int targetCases = 100000;
+        int targetCases = 300000;
 
         int threadNum = 8;
 
@@ -24,14 +28,19 @@ public class TestMainThread {
                 "applicationContext-batch.xml");
         ApplicationDao appDao = (ApplicationDao) context.getBean(ApplicationDao.class);
 
+//        for(int i=0; i<threadNum; i++){
+//            Thread tmpThread = new Thread(new TestCaseInstanceExecutor(targetCases/threadNum, i, appDao, blazeServer));
+//            threadArr[i] = tmpThread;
+//        }
 
-        for(int i=0; i<threadNum; i++){
-            Thread tmpThread = new Thread(new TestCaseInstanceExecutor(targetCases/threadNum, i, appDao, blazeServer));
-            threadArr[i] = tmpThread;
-        }
+        Project project = new Project("cafs",Project.PROJECT_TYPE_XMLBOM);
 
-        for(int i=0; i<threadNum; i++){
-             threadArr[i].start();
-        }
+        Application testCase =  (Application)project.generateTestCase();
+
+        appDao.save(testCase);
+
+//        for(int i=0; i<threadNum; i++){
+//             threadArr[i].start();
+//        }
     }
 }
